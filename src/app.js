86703a68e484144bbff3a6032a0b6539fb850673
LinhDaoNgoc <daongoclinh22@gmail.com>
2022-04-05 22:19:04 +0700

    first commit

---
+++ src/app.js
@@ -0,0 +1,29 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import productRouter from './routes/product';
+import userRouter from './routes/user'
+import cateRouter from './routes/category'
+import postRoter from './routes/posts'
+import sliderRouter from './routes/slider'
+import cors from 'cors'
+// const admin = require('./routes/user');
+const app = express();
+// middleware
+app.use(express.json());
+app.use(cors())
+    //Routing
+app.use("/api", productRouter);
+app.use("/api", userRouter);
+app.use("/api", cateRouter)
+app.use("/api", postRoter);
+app.use("/api", sliderRouter)
+    // connect database
+mongoose.connect("mongodb://localhost:27017/asm-nodejs")
+    .then(() => console.log("Connect db thanh cong"))
+    .catch((error) => console.log(error))
+
+//Connect
+const PORT = 3001;
+app.listen(PORT, () => {
+    console.log(`Sever running port 3001`)
+})
---
+++ src/controllers/auth.js
@@ -0,0 +1,51 @@
+import User from "../models/user";
+import jwt from 'jsonwebtoken';
+export const signup = async(req, res) => {
+    try {
+        const { name, email, password } = req.body;
+        // tai khoan co ton tai hay ko
+        const existUser = await User.findOne({ email }).exec();
+        if (existUser) {
+            return res.status(400).json({
+                message: "Email da ton tai , vui long dang "
+            })
+        }
+        const user = await new User({ name, email, password }).save();
+        res.json({
+            user: {
+                _id: user._id,
+                name: user.name,
+                email: user.email
+            }
+        });
+    } catch (error) {
+        res.status(400).json({
+            message: "Khong tao duoc tai khoan"
+        })
+    }
+
+}
+
+export const signin = async(req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email }).exec();
+    if (!user) {
+        return res.status(401).json({
+            message: "User khong ton tai"
+        })
+    }
+    if (!user.authenticate(password)) {
+        return res.status(401).json({
+            message: "Mat khau khong dung"
+        })
+    }
+    const token = jwt.sign({ _id: user._id }, "123456", { expiresIn: 60 * 60 });
+    return res.json({
+        token,
+        user: {
+            _id: user._id,
+            email: user.email,
+            name: user.name
+        }
+    })
+}
