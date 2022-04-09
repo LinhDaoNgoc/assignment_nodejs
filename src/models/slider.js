ef6430805f768b50ba794dd792b26b4ad1e919f1
LinhDaoNgoc <daongoclinh22@gmail.com>
2022-04-09 11:18:35 +0700

    delete slider

--- src/models/slider.js
+++
@@ -1,7 +0,0 @@
-import mongoose, { Schema } from "mongoose";
-const sliderSchema = new Schema({
-    name: {
-        type: String
-    }
-}, { timestamps: true });
-export default mongoose.model("Slider", sliderSchema);
